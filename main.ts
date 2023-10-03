53a6c7d8a290fb98a0f724fb2d2c3a915e7b12e6
Jack-Eddy <144264179+Jack-Eddy@users.noreply.github.com>
2023-10-03 15:19:06 -0400

    typescript version

--- main.ts
+++ main.ts
@@ -1,8 +1,27 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Jack Eddy
+ * Created on: Oct 2023
+ * This program is a microbit version of cookie clicker
 */
 
-basic.showString('Hello, World!')
+// variables
+let cookies: number
+cookies = 0
+
+// setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+basic.pause(1000)
+
+// add cookie
+input.onButtonPressed(Button.A, function () {
+  basic.showNumber(cookies) 
+  cookies = cookies + 1
+  })
+
+// reset cookies
+input.onButtonPressed(Button.B, function () {
+    cookies = 0
+    basic.showNumber(cookies)
+})
